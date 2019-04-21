ee24c47f37959d74b46d4479827c642243e0c72d
Felix Kunz <felix.kunzjr@gmail.com>
2019-04-21 08:39:34 +0200

    Update TestScript.ino

--- TestScript/TestScript.ino
+++ TestScript/TestScript.ino
@@ -44,17 +44,15 @@
 
 
 
-
-
-
-
-
 void setup() {
   // put your setup code here, to run once:
 
+  DDRC = 0x00;
+Serial.begin(9600);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println(digitalRead(Button1));
 
 }
