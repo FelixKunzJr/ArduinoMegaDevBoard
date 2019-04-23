60fdde4ef53a98470328931bf6dde66e02df0e4a
Felix Kunz <felix.kunzjr@gmail.com>
2019-04-23 19:11:38 +0200

    TEstScript

--- TestScript/TestScript.ino
+++ TestScript/TestScript.ino
@@ -47,12 +47,13 @@
 void setup() {
   // put your setup code here, to run once:
 
+  DDRA = 0xFF;
   DDRC = 0x00;
 Serial.begin(9600);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(digitalRead(Button1));
-
+  
+digitalWrite(i)
 }
